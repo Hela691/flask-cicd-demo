b5a4607f6af67e8f8827c51432c52980189d31de
Hela691 <abaissi.hela@gmail.com>
2025-11-30 15:58:27 +0100

    test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask CI/CD demo!"
+    return "Hello from Flask CI/CD demo! je suis hela"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
